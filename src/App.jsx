88ba131037b41e52b3b2151490ae8cede6b308a4
Miko <jherjati@gmail.com>
2023-06-07 17:43:33 +0700

    impl tile opacity control

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ maplibregl.supported = () => true;
 import DeckGL from "@deck.gl/react";
 import { Tile3DLayer } from "@deck.gl/geo-layers";
 import { useRef } from "react";
+import { useMemo } from "react";
+import { useCallback } from "react";
 // import {
 //   Matrix4,
 //   Vector3,
@@ -56,8 +58,9 @@ export default function App({
   updateAttributions,
 }) {
   const [initialViewState, setInitialViewState] = useState(INITIAL_VIEW_STATE);
+  const [tileOpacity, setTileOpacity] = useState(0.2);
 
-  const onTilesetLoad = (tileset) => {
+  const onTilesetLoad = useCallback((tileset) => {
     // Recenter view to cover the new tileset
     const { cartographicCenter, zoom } = tileset;
     setInitialViewState({
@@ -70,99 +73,99 @@ export default function App({
     if (updateAttributions) {
       updateAttributions(tileset.credits && tileset.credits.attributions);
     }
-  };
+  }, [setInitialViewState]);
 
-  const tile3DLayer = new Tile3DLayer({
+  const tile3DLayer = useMemo(() => new Tile3DLayer({
     id: "tile-3d-layer",
     pointSize: 2,
     data: "/3DHI/test/tileset.json",
-    opacity: 0.25, onTilesetLoad,
+    opacity: tileOpacity, onTilesetLoad,
     pickable: true,
     onClick: (info) => console.log(info)
-  });
+  }), [onTilesetLoad, tileOpacity]);
 
   const mapRef = useRef();
   const [opacity, setOpacity] = useState(1);
   const [showButton, setShowButton] = useState(false);
 
-  /* const onLoad = useCallback(() => {
+  const onLoad = useCallback(() => {
     setShowButton(true);
-    const map = mapRef.current;
-
-    const scene = new Scene();
-    const camera = new PerspectiveCamera();
-    const renderer = new WebGLRenderer({
-      // here we inject our Three.js scene into Mapbox
-      canvas: map.getCanvas(),
-      antialias: true,
-    });
-    renderer.autoClear = false;
-
-    const customLayer = {
-      id: "3d-model",
-      type: "custom",
-      renderingMode: "3d",
-
-      onAdd: function () {
-        //load model
-        const ifcLoader = new IFCLoader();
-        ifcLoader.ifcManager.setWasmPath("/web-ifc/");
-        ifcLoader.load("/BHI.ifc", function (model) {
-          scene.add(model);
-        });
-
-        //add lighting
-        const directionalLight = new DirectionalLight(0x404040);
-        const directionalLight2 = new DirectionalLight(0x404040);
-        const ambientLight = new AmbientLight(0x404040, 3);
-        directionalLight.position.set(0, -70, 100).normalize();
-        directionalLight2.position.set(0, 70, 100).normalize();
-
-        scene.add(directionalLight, directionalLight2, ambientLight);
-      },
-
-      render: function (gl, matrix) {
-        //apply model transformations
-        const rotationX = new Matrix4().makeRotationAxis(
-          new Vector3(1, 0, 0),
-          modelTransform.rotateX
-        );
-        const rotationY = new Matrix4().makeRotationAxis(
-          new Vector3(0, 1, 0),
-          modelTransform.rotateY
-        );
-        const rotationZ = new Matrix4().makeRotationAxis(
-          new Vector3(0, 0, 1),
-          modelTransform.rotateZ
-        );
-
-        const m = new Matrix4().fromArray(matrix);
-        const l = new Matrix4()
-          .makeTranslation(
-            modelTransform.translateX,
-            modelTransform.translateY,
-            modelTransform.translateZ
-          )
-          .scale(
-            new Vector3(
-              modelTransform.scale,
-              -modelTransform.scale,
-              modelTransform.scale
-            )
-          )
-          .multiply(rotationX)
-          .multiply(rotationY)
-          .multiply(rotationZ);
-
-        camera.projectionMatrix = m.multiply(l);
-        renderer.resetState();
-        renderer.render(scene, camera);
-        map.triggerRepaint();
-      },
-    };
-
-    map.getMap().addLayer(customLayer);
-  }, []) */
+    /*  const map = mapRef.current;
+ 
+     const scene = new Scene();
+     const camera = new PerspectiveCamera();
+     const renderer = new WebGLRenderer({
+       // here we inject our Three.js scene into Mapbox
+       canvas: map.getCanvas(),
+       antialias: true,
+     });
+     renderer.autoClear = false;
+ 
+     const customLayer = {
+       id: "3d-model",
+       type: "custom",
+       renderingMode: "3d",
+ 
+       onAdd: function () {
+         //load model
+         const ifcLoader = new IFCLoader();
+         ifcLoader.ifcManager.setWasmPath("/web-ifc/");
+         ifcLoader.load("/BHI.ifc", function (model) {
+           scene.add(model);
+         });
+ 
+         //add lighting
+         const directionalLight = new DirectionalLight(0x404040);
+         const directionalLight2 = new DirectionalLight(0x404040);
+         const ambientLight = new AmbientLight(0x404040, 3);
+         directionalLight.position.set(0, -70, 100).normalize();
+         directionalLight2.position.set(0, 70, 100).normalize();
+ 
+         scene.add(directionalLight, directionalLight2, ambientLight);
+       },
+ 
+       render: function (gl, matrix) {
+         //apply model transformations
+         const rotationX = new Matrix4().makeRotationAxis(
+           new Vector3(1, 0, 0),
+           modelTransform.rotateX
+         );
+         const rotationY = new Matrix4().makeRotationAxis(
+           new Vector3(0, 1, 0),
+           modelTransform.rotateY
+         );
+         const rotationZ = new Matrix4().makeRotationAxis(
+           new Vector3(0, 0, 1),
+           modelTransform.rotateZ
+         );
+ 
+         const m = new Matrix4().fromArray(matrix);
+         const l = new Matrix4()
+           .makeTranslation(
+             modelTransform.translateX,
+             modelTransform.translateY,
+             modelTransform.translateZ
+           )
+           .scale(
+             new Vector3(
+               modelTransform.scale,
+               -modelTransform.scale,
+               modelTransform.scale
+             )
+           )
+           .multiply(rotationX)
+           .multiply(rotationY)
+           .multiply(rotationZ);
+ 
+         camera.projectionMatrix = m.multiply(l);
+         renderer.resetState();
+         renderer.render(scene, camera);
+         map.triggerRepaint();
+       },
+     };
+ 
+     map.getMap().addLayer(customLayer); */
+  }, [])
 
   return (
     <>
@@ -192,33 +195,37 @@ export default function App({
           preventStyleDiffing
           mapLib={maplibregl}
           mapStyle={mapStyle}
-        // onLoad={onLoad}
+          onLoad={onLoad}
         ></Map>
       </DeckGL>
       {showButton && (
-        <button
-          style={{ position: "absolute", right: 16, top: 16 }}
-          onClick={(event) => {
-            event.preventDefault();
-            const map = mapRef.current;
-            const currentStyle = map.getStyle();
-            const visibleLayers = currentStyle.layers
-              .map((l) => l)
-              .filter((layer) => layer.layout?.visibility !== "none");
-            try {
-              for (const { id, type } of visibleLayers) {
-                map
-                  .getMap()
-                  .setPaintProperty(id, type + "-opacity", (opacity + 0.2) % 1);
+        <div style={{ position: "absolute", right: 16, top: 16 }}>
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              const map = mapRef.current;
+              const currentStyle = map.getStyle();
+              const visibleLayers = currentStyle.layers
+                .map((l) => l)
+                .filter((layer) => layer.layout?.visibility !== "none");
+              try {
+                for (const { id, type } of visibleLayers) {
+                  map
+                    .getMap()
+                    .setPaintProperty(id, type + "-opacity", (opacity + 0.2) % 1);
+                }
+                setOpacity((opacity + 0.2) % 1);
+              } catch (error) {
+                console.log(error);
               }
-              setOpacity((opacity + 0.2) % 1);
-            } catch (error) {
-              console.log(error);
-            }
-          }}
-        >
-          Change basemap opacity
-        </button>
+            }}
+          >
+            Change basemap opacity
+          </button>
+          <button style={{ marginLeft: 6 }} onClick={() => {
+            setTileOpacity(prev => prev === 0.8 ? 1 : (prev + 0.2) % 1)
+          }}>Change tile opacity</button>
+        </div>
       )}
     </>
   );
